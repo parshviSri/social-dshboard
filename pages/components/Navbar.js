80533f800e8e1506901afebe468a95e780488947
parshviSri <parshvisri@gmail.com>
2022-12-18 19:15:31 +0530

    navbar added
    Signed-off-by: parshviSri <parshvisri@gmail.com>

---
+++ pages/components/Navbar.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+      <div className='flex justify-end text-xl p-6 mr-5'>
+        <Link href="/" className='p-6'>Home</Link>
+        <Link href="/dashboard" className='p-6'>Dashboard</Link>
+        <Link href="/aboutUs" className='p-6'>About Us</Link>
+      </div>
+    );
+}
+
+export default Navbar;
