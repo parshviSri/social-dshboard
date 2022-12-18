12af7ef35d95d9a38bb16c3b8decfcd0ad112fa6
parshviSri <parshvisri@gmail.com>
2022-12-18 19:41:53 +0530

    Introduction
    Signed-off-by: parshviSri <parshvisri@gmail.com>

---
+++ pages/components/landing-page/introduction.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Introduction = () => {
+    return (
+      <div className="flex text-center font-mono">
+        <div className="basis-1/2">
+          <p className="text-7xl ">Social Dashboard</p>
+          <p className="text-4xl p-6">
+            One place for all the social media handles.
+          </p>
+        </div>
+        <div className="basis-1/2"></div>
+      </div>
+    );
+}
+
+export default Introduction;
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,29 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Introduction from './components/landing-page/introduction';
 import Navbar from './components/Navbar'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="bg-[#D1BAFE]">
       <Head>
         <title>Social Dshboard</title>
-        <meta name="description" content="platform for all the social media content" />
+        <meta
+          name="description"
+          content="platform for all the social media content"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
-      <main className={styles.main}>
-        <Navbar/>
+
+      <main>
+        <Navbar />
+        <div className='my-8'>
+          <Introduction />
+        </div>
       </main>
 
-      <footer className={styles.footer}>
-        
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
-  )
+  );
 }
