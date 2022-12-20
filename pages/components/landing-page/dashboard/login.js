b445d42c73796824ba7a03f70672775c0147ac9a
parshviSri <parshvisri@gmail.com>
2022-12-20 14:25:40 +0530

    fixing login prompt
    Signed-off-by: parshviSri <parshvisri@gmail.com>

--- pages/components/landing-page/dashboard/login.js
+++ pages/components/landing-page/dashboard/login.js
@@ -4,7 +4,7 @@ const LoginElement = () => {
     return (
       <div className="flex justify-center m-6">
         <div className="basis-2.5/3">
-          <p className="text-7xl">Start your new digital life</p>
+          <p className="text-4xl">Start your new digital life</p>
           <div className="flex justify-center m-12">
             <button className="text-3xl border rounded-md p-4 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 px-12 ">
               Connect with google
