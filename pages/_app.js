8dba0a7e0fcbf3de988db62f31cfdc7c4ec1dedb
parshviSri <parshvisri@gmail.com>
2023-01-26 21:12:13 +0530

    adding analytics
    Signed-off-by: parshviSri <parshvisri@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,11 @@ import configureStore from "../state/store";
 import { Provider } from "react-redux";
 import Navbar from './components/Navbar';
 import Contact from './components/Contact';
+import { Analytics } from "@vercel/analytics/react";
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={configureStore}>
+        <Analytics/>
           <Navbar/>
           <Component {...pageProps} />
     </Provider>
